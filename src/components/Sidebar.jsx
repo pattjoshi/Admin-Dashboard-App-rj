411f900d9924bf520b3eb46757b36e01818779f6
om praksash pattjoshi <omprakashpattjoshi123@gmail.com>
2022-05-28 13:47:01 +0530

    compited context Provider

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,16 +6,18 @@ import { MdOutlineCancel } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { links } from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const activeMenue = true;
+  const { activeMenu, setActiveMenu } = useStateContext();
+
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
   const normalLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
-      {activeMenue && (
+      {activeMenu && (
         <>
           <div className="flex justify-between items-center">
             <Link
@@ -28,7 +30,7 @@ const Sidebar = () => {
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
                 type="button"
-                onClick={() => {}}
+                onClick={() => setActiveMenu(false)}
                 style={{ color: "currentcolor" }}
                 className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
               >
